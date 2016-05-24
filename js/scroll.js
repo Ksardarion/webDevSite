ac790c3c3465a998a36aac3b43fd41aa30b87262
Ksardarion <cool.ksandr2013@gmail.com>
2016-05-24 18:23:24 +0300

    Some fixes&update
    Added multilanguage plugin;
    Some changes in design

--- js/scroll.js
+++ js/scroll.js
@@ -3,9 +3,11 @@ $( document ).ready( function() {
 });
 
 function scrollTo () {
-  $('#next-section').click(function(e) {
-      e.preventDefault();
-     var distanceTopToSection = $( '#' +  $(this).data('target')).offset().top;
+	var menuHeight = $('nav').height();
+
+  $('.scrollTo').click(function(e) {
+     e.preventDefault();
+     var distanceTopToSection = $( '#' +  $(this).data('target')).offset().top - menuHeight;
 
      // particlesJS..particles.number.value = 0;
      // console.log(particles.particles.number.value);
