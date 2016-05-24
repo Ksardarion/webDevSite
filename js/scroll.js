235a675cf33cd0ff860b1bdacf8423815da90413
Olecksandr <velsashok@gmail.com>
2016-05-24 18:47:56 +0300

    select menu item animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,42 @@
 $( document ).ready( function() {
-  scrollTo();
+
+	// ===================== Для анімації виділення пункта меню
+	var menuPointer = $('#menu-pointer');
+	var currentPage = $('#current-page');
+
+	menuPointer.css('width', currentPage.outerWidth());
+	menuPointer.css('left', currentPage.offset().left);
+
+	$(window).on('resize', function() {
+		menuPointer.css('width', currentPage.outerWidth());
+		menuPointer.css('left', currentPage.offset().left);
+	});
+
+	var menuElements = $('nav a');
+	menuElements.click(function(e) {
+		e.preventDefault();
+
+		currentPage.removeAttr('id');
+		currentPage = $(this);
+		currentPage.attr('id', 'current-page');
+
+		menuPointer.animate({
+			left: currentPage.offset().left,
+			width: currentPage.outerWidth()
+		}, 500);
+	});
+
+
+	// ===================== Ф-ція для плавного скролу до потрібного елемента
+	scrollTo();
 });
 
 function scrollTo () {
-  $('#next-section').click(function(e) {
+	var menuHeight = $('nav').height();
+
+  $('.scrollTo').click(function(e) {
       e.preventDefault();
-     var distanceTopToSection = $( '#' +  $(this).data('target')).offset().top;
+     var distanceTopToSection = $( '#' +  $(this).data('target')).offset().top - menuHeight;
 
      // particlesJS..particles.number.value = 0;
      // console.log(particles.particles.number.value);
